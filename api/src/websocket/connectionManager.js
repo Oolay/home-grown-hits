179ea75460741d35f3ece73c8b2a90aceb74d2b2
Oolay <alex.angell@my.jcu.edu.au>
2020-05-24 19:39:22 +1000

    set up websocket connection

--- api/src/websocket/connectionManager.js
+++ api/src/websocket/connectionManager.js
@@ -5,19 +5,19 @@ const AWS = require('aws-sdk')
 const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-10-08' })
 const connectionTableName = 'homeGrownHitsConnections'
 
-function saveConnection(gameId, connection) {
+function saveConnection(connection, gameId) {
     const putParams = {
         TableName: connectionTableName,
         Item: {
+            connectionId: connection.connectionId,
             gameId,
-            connectionId: connection.connectionId
         }
     }
 
     return documentClient.put(putParams).promise()
 }
 
-function removeConnection(gameId, connection) {
+function removeConnection({ connectionId }) {
     const deleteParams = {
         TableName: connectionTableName,
         Key: {
@@ -29,13 +29,18 @@ function removeConnection(gameId, connection) {
 }
 
 module.exports.connectionManager = async event => {
+    console.info('event', JSON.stringify(event))
     try {
         if (event.requestContext.eventType === 'CONNECT') {
+            const { gameId } = event.queryStringParameters
+
             const connection = {
                 connectionId : event.requestContext.connectionId,
             }
 
-            await saveConnection(connection)
+            console.log('gameId', gameId, 'connectionId', event.requestContext.connectionId)
+
+            await saveConnection(connection, gameId)
 
             return {
                 statusCode: 200,
